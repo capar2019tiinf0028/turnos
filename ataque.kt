7958219c8267ca02370c602e062061465b8d7555
ANDREW DYLAN VIANA AMORIM <20191TIINF0028@ifpi.int>
2022-05-06 10:23:00 -0300

    começando a implementar o ataque

--- ataque.kt
+++ ataque.kt
@@ -1,5 +1,11 @@
 package turnos
 
-fun ataque(turno: ResumoDoTurno, atacante: Entidade, alvo: Entidade, bonus) {
-    
+
+//calcula os ataques e coloca no resumo do turno
+//conforme a seguientes regras:
+//-> se no número gerado for 
+fun ataque(turno: ResumoDoTurno, atacante: Entidade, alvo: Entidade, bonus: Int) {
+    val critico: Int
+
+    if 
 }
